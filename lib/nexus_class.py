00427be9d98acb53ff49638556a1c88904e4eba6
Mikael Sundberg <misu02@me.com>
2017-05-12 21:31:55 +0200

    Tests and class created

---
+++ lib/nexus_class.py
@@ -0,0 +1,48 @@
+class NexusHelperClass:
+    """A class of nexus functions"""
+    def __init__(self, url):
+        self.url = url
+
+    @staticmethod
+    def get_leverables():
+        """
+        Gets all groups (leverables) from nexus
+        :return: list
+        """
+
+        leverables = []
+
+        return leverables
+
+    @staticmethod
+    def get_artefacts(leverables):
+        """
+        Gets all artefacts from all leverables in nexus
+        
+        :param leverables: 
+        :type leverables list
+        :return: dict
+        """
+
+        for lev in leverables:
+            print(lev)
+
+        artefacts = {}
+
+        return artefacts
+
+    @staticmethod
+    def get_artefactversions(artefacts):
+        """
+        Gets all artefact version from artefacts
+        
+        :param artefacts:
+        :type artefacts dict
+        :return: dict of dicts 
+        """
+        for artefact in artefacts:
+            print(artefact)
+
+        artefactversions = {}
+
+        return artefactversions
---
+++ tests/nexus_test.py
@@ -0,0 +1,24 @@
+import unittest
+from lib.nexus_class import NexusHelperClass
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        self.nexusHelper = NexusHelperClass('url')
+
+    def test_get_nexus_leverables(self):
+
+        self.assertIsInstance(self.nexusHelper.get_leverables(), list)
+
+    def test_get_nexus_artefacts(self):
+
+        self.assertIsInstance(self.nexusHelper.get_artefacts([]), dict)
+
+    def test_get_nexus_artefactversions(self):
+
+        self.assertIsInstance(self.nexusHelper.get_artefactversions({}), dict)
+
+if __name__ == '__main__':
+    unittest.main()
